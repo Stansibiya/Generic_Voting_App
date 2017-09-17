334aebd91a003d883cca7fdb89b260da17f1767b
Tshepo_sibiya <Tshepo.sibiya@gmail.com>
2017-09-17 09:38:44 +0200

    New generic web App!

---
+++ Generic_Voting_App/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Generic_Voting_App
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Generic_Voting_App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Generic_Voting_App.Startup))]
+namespace Generic_Voting_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
